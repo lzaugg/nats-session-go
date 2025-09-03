bd52cab80c8baf4dd1fe596241756b15c5965e66
Lukas Zaugg <l.zaugg@mypi.ch>
2025-09-03 17:02:55 +0200

    add jetstream example

--- 03-nats-jetstream/a-stream/main.go
+++ 03-nats-jetstream/a-stream/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
+	"github.com/eiannone/keyboard"
 	"github.com/joho/godotenv"
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 )
 
 func main() {
@@ -38,12 +42,80 @@ func main() {
 	}
 	defer nc.Close()
 
-	subject := "example.*.position"
-	slog.Info("subscribing to " + subject)
-	nc.Subscribe(subject, func(m *nats.Msg) {
-		fmt.Println("received message", "subject", m.Subject, "data", string(m.Data))
-	})
+	/*
+		// old way to get jetstream context (kind of deprecated)
+		_, err = nc.JetStream()
+		if err != nil {
+			slog.Error("error getting jetstream context", "error", err.Error())
+			os.Exit(1)
+		}
+	*/
 
-	select {}
+	// new way
+	js, err := jetstream.New(nc)
+	if err != nil {
+		slog.Error("error getting jetstream context", "error", err.Error())
+		os.Exit(1)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	streamName := fmt.Sprintf("POSITION_%s", user)
+	stream, err := js.CreateStream(ctx, jetstream.StreamConfig{
+		Name:     streamName,
+		Subjects: []string{"example.*.position"},
+	})
+	if err != nil {
+		slog.Error("error creating stream", "error", err.Error())
+		os.Exit(1)
+	}
+
+	cons, err := stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+		DeliverPolicy: jetstream.DeliverAllPolicy,
+		Durable:       "position-consumer-" + user,
+	})
+	if err != nil {
+		slog.Error("error creating consumer", "error", err.Error())
+		os.Exit(1)
+	}
+
+	keysEvents, err := keyboard.GetKeys(10)
+	if err != nil {
+		slog.Error("error getting keys", "error", err.Error())
+		os.Exit(1)
+	}
+	defer keyboard.Close()
+
+	paused := false
+	for {
+
+		select {
+		case key := <-keysEvents:
+			if key.Key == keyboard.KeyCtrlC {
+				slog.Info("ctrl+c pressed, exiting")
+				os.Exit(0)
+			}
+			if key.Rune == 'p' || key.Rune == 'P' {
+				slog.Info("handle key event", "paused", paused)
+				paused = !paused
+			}
+		default:
+			if paused {
+				continue
+			}
+			msgs, _ := cons.Fetch(1)
+			var i int
+			for msg := range msgs.Messages() {
+				msg.Ack()
+				metadata, _ := msg.Metadata()
+				if err != nil {
+					slog.Error("error getting metadata", "error", err.Error())
+					continue
+				}
+				fmt.Println("received message", "subject", msg.Subject(), "seq", metadata.Sequence, "data", string(msg.Data()))
+				i++
+			}
+		}
+	}
 
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module nats-session-go
 go 1.24.2
 
 require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 	github.com/joho/godotenv v1.5.1
 	github.com/nats-io/nats.go v1.45.0
 )
